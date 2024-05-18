a7c9dd10c65e073cc30246d0c4b2563d8b1c8bf2
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-18 20:50:29 +0700

    Lab11 Dynamic Type: Fundamental2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+let a = undefined;
+let b = null;
+let c = b + '4 2';
+
+let d = Number(a)
+console.log(d)          // NaN เพราะ undefined ถูกแปลงเป็น Number
+console.log(typeof(d))           
+
+let e = Number(b)
+console.log(e)          // 0 เพราะแปลงเป็น Number
+console.log(typeof(e))          
+
+let f = Number(c)
+console.log(f)          // Nan เพราะ null + string ถูกค่าแล้วแปลงเป็น Number
+console.log(typeof(f))  
+
